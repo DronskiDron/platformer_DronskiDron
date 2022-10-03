8fe10466c2e18bc1d2daafe4ad8167ffc4893558
DronskiDron <andronaf93@gmail.com>
2022-10-03 20:05:17 +0300

    Домашняя работа №2/2

---
+++ platformer_DronskiDron(Andrey)/Assets/Scripts/Player/PlayerControllerModernVersionFirst.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerControllerModernVersionFirst : MonoBehaviour
+    {
+        [SerializeField] private float _speed = 5f;
+
+        private float _xDirection;
+        private float _yDirection;
+
+
+        private void Update()
+        {
+            PlayerHorizontalMover();
+            PlayerVerticalMover();
+        }
+
+
+        public void SetDirectionX(float xDirection)
+        {
+            _xDirection = xDirection;
+        }
+
+
+        public void SetDirectionY(float yDirection)
+        {
+            _yDirection = yDirection;
+        }
+
+
+        public void SaySomething()
+        {
+            Debug.Log("ПАЛУНДРА!!!");
+        }
+
+
+        private void PlayerHorizontalMover()
+        {
+            if (_xDirection != 0)
+            {
+                var delta = _xDirection * _speed * Time.deltaTime;
+                var newXPosition = transform.position.x + delta;
+                transform.position = new Vector3(newXPosition, transform.position.y, transform.position.z);
+            }
+        }
+
+
+        private void PlayerVerticalMover()
+        {
+            if (_yDirection != 0)
+            {
+                var delta = _yDirection * _speed * Time.deltaTime;
+                var newYPosition = transform.position.y + delta;
+                transform.position = new Vector3(transform.position.x, newYPosition, transform.position.z);
+            }
+        }
+    }
+}
+
---
+++ platformer_DronskiDron(Andrey)/Assets/Scripts/Player/PlayerInputReaderLegacyUsingAxis.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Player 
+{
+    public class PlayerInputReaderLegacyUsingAxis : MonoBehaviour
+    {
+        [SerializeField] private PlayerControllerLegacy _player;
+
+
+        private void Update()
+        {
+            GetHorizontalMovingInput();
+
+            GetVerticalMovingInput();
+
+            GetSaySomethingInput();
+        }
+
+
+        private void GetHorizontalMovingInput()
+        {
+            var horizontalAxis = Input.GetAxis("Horizontal");
+            _player.SetDirectionX(horizontalAxis);
+        }
+
+        private void GetVerticalMovingInput()
+        {
+            var verticalAxis = Input.GetAxis("Vertical");
+            _player.SetDirectionY(verticalAxis);
+        }
+
+        private void GetSaySomethingInput()
+        {
+            if (Input.GetButtonUp("Fire1"))
+            {
+                _player.SaySomething();
+            }
+        }
+    }
+}
+
